95ae6c6eaa2d47b508a1ec12816ab5b1d682386f
Elena Guzman <elena.guzbla@gmail.com>
2019-10-11 14:02:24 +0200

    :art: Add Project control

---
+++ idiTasks/idiTasks.Shared/Controls/ProjectItemControl.cs
@@ -0,0 +1,12 @@
+﻿using Windows.UI.Xaml.Controls;
+
+namespace idiTasks.Shared.Controls
+{
+    public sealed partial class ProjectItemControl : UserControl
+    {
+        public ProjectItemControl()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
